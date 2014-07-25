286851efb26a57918aa6c3be36cd10667f98eef2
Federico Ragona <federico.ragona@gmail.com>
2014-07-25 23:21:54 +0200

    Basic functionalities for Player, Deck

--- src/main/scala/com/github/fedragon/dominion/Dominion.scala
+++ src/main/scala/com/github/fedragon/dominion/Dominion.scala
@@ -1,5 +1,7 @@
 package com.github.fedragon.dominion
 
+import scala.util.Random
+
 object Dominion {
 
   sealed trait Card {
@@ -10,35 +12,79 @@ object Dominion {
     override def toString = name
   }
 
+  type Cards = Vector[Card]
+
   sealed trait CardType
-  object Treasure extends CardType
+
+  case object Treasure extends CardType
+
+  case object Victory extends CardType
 
   case class CardValue(value: Int) extends AnyVal
+
   case class Coins(value: Int) extends AnyVal
 
   abstract class Treasure(val name: String, val typ: CardType, val cost: Coins, val value: CardValue) extends Card
-  object Copper extends Treasure("Copper", typ = Treasure, cost = Coins(0), value = CardValue(1))
-  object Silver extends Treasure("Silver", typ = Treasure, cost = Coins(3), value = CardValue(5))
-  object Gold extends Treasure("Gold", typ = Treasure, cost = Coins(6), value = CardValue(8))
 
-  case class Deck private(cards: Map[Card, Int]) {
+  case object Copper extends Treasure("Copper", typ = Treasure, cost = Coins(0), value = CardValue(1))
 
-    def -=(card: Card, amount: Int) = {
-      val newAmount = cards.get(card).fold(0)(_ - amount)
-      new Deck(cards.updated(card, newAmount))
+  //  case object Silver extends Treasure("Silver", typ = Treasure, cost = Coins(3), value = CardValue(5))
+  //
+  //  case object Gold extends Treasure("Gold", typ = Treasure, cost = Coins(6), value = CardValue(8))
+
+  abstract class Victory(val name: String, val typ: CardType, val cost: Coins, val value: CardValue) extends Card
+
+  case object Estate extends Victory("Estate", typ = Victory, cost = Coins(2), value = CardValue(1))
+
+  case class Deck private[dominion](cards: Cards) {
+    def draw: Option[(Card, Deck)] =
+      if (cards.isEmpty) None
+      else Some((cards.head, copy(cards.tail)))
+
+    def insert(cs: Cards) = Deck(cards ++ cs)
+
+    def shuffle: Deck = {
+      println("Shuffling")
+      copy(Random.shuffle(cards))
     }
-
   }
 
   object Deck {
     def apply(): Deck = {
-      val cards = Map[Card, Int](
-        Copper -> 60,
-        Silver -> 40,
-        Gold -> 30
-      )
+      val cards =
+        Vector.fill(60)(Copper) ++
+          //          Vector.fill(40)(Silver) ++
+          //          Vector.fill(30)(Gold) ++
+          Vector.fill(24)(Estate)
 
       new Deck(cards)
     }
   }
+
+  case class Player(name: String, hand: Cards, discarded: Cards, deck: Deck) {
+
+    def this(name: String, deck: Deck) = this(name, Vector.empty, Vector.empty, deck)
+
+    def canBuy(that: Card) = {
+      val coins = hand.count {
+        case _: Treasure => true
+        case _ => false
+      }
+
+      coins >= that.cost.value
+    }
+
+    def draw: Player = {
+      deck.draw match {
+        case Some((card, newDeck)) =>
+          copy(hand = card +: hand, deck = newDeck)
+        case None =>
+          val (card, newDeck) = deck.insert(discarded).shuffle.draw.get
+          copy(hand = card +: hand, discarded = Vector.empty, deck = newDeck)
+      }
+    }
+
+    def discard: Player = copy(hand = Vector.empty, discarded = discarded ++ hand)
+  }
+
 }
--- src/test/scala/com/github/fedragon/dominion/DominionSpec.scala
+++ src/test/scala/com/github/fedragon/dominion/DominionSpec.scala
@@ -4,12 +4,39 @@ class DominionSpec extends UnitSpec {
 
   import Dominion._
 
-  val subject = Deck()
 
-  "A deck" should "allow to draw cards" in {
-    subject.cards shouldBe Map[Card, Int](Copper -> 60, Silver -> 40, Gold -> 30)
+  "Drawing cards from a deck" should "reduce the number of remaining cards" in {
+    val subject = Deck()
+    subject.cards.size shouldBe 84
+    val (_, deck) = subject.draw.get
 
-    val updated = subject -= (Copper, 3) -= (Silver, 6) -= (Gold, 13)
-    updated.cards shouldBe Map[Card, Int](Copper -> 57, Silver -> 34, Gold -> 17)
+    deck.cards.size shouldBe 83
+  }
+
+  "A player" should "be able to buy a card if he has enough coins" in {
+    val subject = Player("Player", Vector.fill(7)(Copper), Vector.empty, Deck(Vector.empty))
+    subject.canBuy(Estate) shouldBe true
+
+    val poor = Player("Poor", Vector.fill(1)(Copper), Vector.empty, Deck(Vector.empty))
+    poor.canBuy(Estate) shouldBe false
+  }
+
+  it should "be able to draw from his deck" in {
+    val first = new Player("Player", Deck(Vector(Copper, Estate))).draw
+    first.hand should contain (Copper)
+    first.deck.cards should contain only Estate
+
+    val second = first.draw
+    second.deck.cards shouldBe 'empty
+    second.hand should contain only (Copper, Estate)
+  }
+
+  it should "be able to discard from his hand" in {
+    val first = new Player("Player", Deck(Vector(Copper, Estate))).draw
+    first.hand should contain(Copper)
+
+    val second = first.discard
+    second.hand shouldBe 'empty
+    second.discarded should contain(Copper)
   }
 }
