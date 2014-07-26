5a95b439092eeb6e49128eb34720bf10d3f260e5
Federico Ragona <federico.ragona@gmail.com>
2014-07-26 08:07:50 +0200

    A player should not be allowed more actions than he can play

--- src/test/scala/com/github/fedragon/dominion/DominionSpec.scala
+++ src/test/scala/com/github/fedragon/dominion/DominionSpec.scala
@@ -8,50 +8,59 @@ class DominionSpec extends UnitSpec {
   import VictoryCards._
 
   "Drawing cards from a deck" should "reduce the number of remaining cards" in {
-    val subject = Deck(Vector(Copper))
-    subject.cards.size shouldBe 1
-    val (_, deck) = subject.draw.get
+    val deck = Deck(Vector(Copper))
+    deck.cards.size shouldBe 1
+    val (_, newDeck) = deck.draw.get
 
-    deck.cards shouldBe 'empty
+    newDeck.cards shouldBe 'empty
   }
 
   "A player" should "be able to buy a card if he has enough coins" in {
-    val subject = Player("Player", Vector.fill(7)(Copper), Vector.empty, Deck(Vector.empty))
-    subject.canBuy(Estate) shouldBe true
+    val player = Player("Player", Vector.fill(7)(Copper), Vector.empty, Deck(Vector.empty))
+    player.canBuy(Estate) shouldBe true
 
     val poor = Player("Poor", Vector.fill(1)(Copper), Vector.empty, Deck(Vector.empty))
     poor.canBuy(Estate) shouldBe false
   }
 
   it should "be able to draw from his deck" in {
-    val first = new Player("Player", deck = Deck(Vector(Copper, Estate))).draw
-    first.hand should contain (Copper)
-    first.deck.cards should contain only Estate
+    val stateOne = new Player("Player", deck = Deck(Vector(Copper, Estate))).draw
+    stateOne.hand should contain (Copper)
+    stateOne.deck.cards should contain only Estate
 
-    val second = first.draw
-    second.deck.cards shouldBe 'empty
-    second.hand should contain only (Copper, Estate)
+    val stateTwo = stateOne.draw
+    stateTwo.deck.cards shouldBe 'empty
+    stateTwo.hand should contain only (Copper, Estate)
   }
 
   it should "be able to discard from his hand" in {
-    val first = new Player("Player", deck = Deck(Vector(Copper, Estate))).draw
-    first.hand should contain(Copper)
+    val stateOne = new Player("Player", deck = Deck(Vector(Copper, Estate))).draw
+    stateOne.hand should contain(Copper)
 
-    val second = first.discard
-    second.hand shouldBe 'empty
-    second.discarded should contain(Copper)
+    val stateTwo = stateOne.discard
+    stateTwo.hand shouldBe 'empty
+    stateTwo.discarded should contain(Copper)
   }
 
   it should "play an action, if there is at least one in his hand" in {
-    val first = Player("Player", hand = Vector(Smithy), deck = Deck(Vector(Copper, Estate, Copper)))
-    val second = first.playAction
+    val stateOne = Player("Player", hand = Vector(Smithy), deck = Deck(Vector(Copper, Estate, Copper)))
+    val stateTwo = stateOne.plays
 
-    second.hand.size shouldBe 4
-    second.deck.cards shouldBe 'empty
+    stateTwo.hand.size shouldBe 4
+    stateTwo.deck.cards shouldBe 'empty
   }
 
   it should "not play an action, if there are not any in his hand" in {
-    val first = new Player("Player", deck = Deck(Vector(Copper, Estate)))
-    first.playAction shouldBe first
+    val player = new Player("Player", deck = Deck(Vector(Copper, Estate)))
+    player.plays shouldBe player
+  }
+
+  it should "not be allowed to play more actions than he can" in {
+    val stateOne = Player("Player", hand = Vector(Smithy), deck = Deck(Vector(Copper, Estate, Copper)))
+    val stateTwo = stateOne.plays
+    val stateThree = stateTwo.plays
+
+    stateOne should not equal stateTwo
+    stateTwo shouldEqual stateThree
   }
 }
